8868a21ede3da13b9a4cc9629d999901dd194917
Darius <dariuskumza@gmail.com>
2024-02-26 19:09:16 +0200

    fetch spinner added

--- src/components/products/AllProducts.jsx
+++ src/components/products/AllProducts.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box, Container, LinearProgress } from '@mui/material';
 import { useState } from 'react';
+import axios from 'axios';
+import { useAuthContext } from '../../context/autCtx';
+
+const PRODUCT_URL = 'http://localhost:3000/api/products';
 
 export const AllProducts = () => {
-  const [spinner, setSpinner] = useState(false);
+  const [spinner, setSpinner] = useState(true);
+  const { token } = useAuthContext();
+
+  useEffect(() => {
+    axios
+      .get(PRODUCT_URL, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setSpinner(false);
+      })
+      .catch((error) => {
+        console.log('error ===', error);
+        setSpinner(false);
+      });
+  }, []);
   return (
     <>
-      {/* <Box sx={{ width: '100%', position: 'absolute' }}>
-        {!spinner && <LinearProgress />}
-      </Box> */}
+      <Box sx={{ width: '100%', position: 'absolute' }}>
+        {spinner && <LinearProgress />}
+      </Box>
 
-      <Container maxWidth="lg">
-        <h1>AllProducts</h1>
-      </Container>
+      <Container maxWidth="lg"></Container>
     </>
   );
 };
